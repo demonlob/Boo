addff88f0659dc42826af3cc49b80f5b0491976a
demonlob <142598456+demonlob@users.noreply.github.com>
2023-12-22 11:58:20 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -57,9 +57,9 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Pranav-Saraswat/Testing-Alpha",
+    "https://github.com/demonlob/Boo",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "hydro")
 
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv("GIT_TOKEN", None)
